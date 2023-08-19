748ede1b29a0c0acd33407addd0bae1cc16b93f0
Y.W <130629956+Y-Watanabe-Eng@users.noreply.github.com>
2023-08-19 16:45:42 +0900

    Bottom.jsx の更新

--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -7,7 +7,7 @@ export const Bottom = () => {
     <div className='bottom'>
         <nav>
 
-            <ul className='bottom-nav'>
+            <ul className='bottom-nav grid-cols-3'>
                 <li><Link className='text-sm text-white hover:text-green-800' to="/Works/">PORTFOLIO</Link></li>
                 <li><Link className='text-sm text-white hover:text-green-800' to="/Works/Skill">SKILL</Link></li>
                 <li><Link className='text-sm text-white hover:text-green-800' to="/Works/Profile">PROFILE</Link></li>
